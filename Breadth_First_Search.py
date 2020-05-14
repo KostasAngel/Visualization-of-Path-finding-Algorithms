f019e3927b84623a5a0909f159517d4ee671d457
marios <msthoma@yahoo.com>
2020-05-14 11:09:02 +0300

    add function for getting neighbors

--- Breadth_First_Search.py
+++ Breadth_First_Search.py
@@ -1,5 +1,38 @@
+import json
+from collections import defaultdict
+from collections import deque
+
+
+# inspired by this gist: https://gist.github.com/hrldcpr/2012250
+def tree(): return defaultdict(tree)
+
+
+def get_neighbors(point, grid):
+    # possible movements, only up down left right, maybe diagonally should be an option?
+    dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+    neighbors = []
+    for i in range(4):
+        # possible neighbors
+        x, y = point[0] + dx[i], point[1] + dy[i]
+
+        # check if neighbors are within the grid, skip if not
+        if not (0 <= x < len(grid[0]) and 0 <= y < len(grid)):
+            continue
+
+        neighbors.append((x, y))
+
+    return neighbors
+
+
 def main():
-    pass
+    # to store visited points, and allow to determine the shortest path at the end
+    traversed_tree = tree()
+
+    # double ended queue, to use as a FIFO queue
+    q = deque()
+
+    print(json.dumps(traversed_tree, indent=4))
 
 
 if __name__ == '__main__':
