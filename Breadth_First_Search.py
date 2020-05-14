6580d2dee64b8712daa25fc094056780dab0ffdc
marios <msthoma@yahoo.com>
2020-05-14 14:19:09 +0300

    first try at algorithm

--- Breadth_First_Search.py
+++ Breadth_First_Search.py
@@ -1,9 +1,21 @@
+import curses
+import time
 from collections import defaultdict
 from collections import deque
 
+import numpy as np
 
-def new_grid(dim):
-    return [["." for _ in range(dim)] for _ in range(dim)]
+
+def print_grid(visited, dim):
+    empty_grid = np.array(new_grid(" ", dim))
+    for p in visited:
+        empty_grid[p] = "."
+
+    return "\n".join(" ".join(i) for i in empty_grid)
+
+
+def new_grid(fill, dim):
+    return [[f"{fill}" for _ in range(dim)] for _ in range(dim)]
 
 
 # inspired by this gist: https://gist.github.com/hrldcpr/2012250
@@ -31,11 +43,39 @@ def get_neighbors(point, grid):
 def main():
     # to store visited points, and allow to determine the shortest path at the end
     traversed_tree = tree()
+    grid_dim = 20
+    grid = new_grid(".", grid_dim)
+
+    start = (0, 0)
+    goal = (9, 9)
 
     # double ended queue, to use as a FIFO queue
-    q = deque()
+    q = deque([start])
 
-    print(new_grid(4))
+    visited = []
+
+    screen = curses.initscr()
+
+    while q:  # checks if q is empty
+        current_point = q.popleft()
+
+        visited.append(current_point)  # mark as visited
+
+        neighbors = get_neighbors(current_point, grid)
+        # print(current_point, neighbors)
+
+        for neighbor in neighbors:
+
+            if neighbor not in visited:  # check if already visited this point
+                if neighbor not in q:
+                    q.append(neighbor)
+
+                    screen.clear()
+                    screen.addstr(print_grid(visited, grid_dim))
+                    screen.refresh()
+                    time.sleep(0.05)
+
+        # print(current_point, visited)
 
 
 if __name__ == '__main__':
