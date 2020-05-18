d69ecd7cd45967ae8e0f018870443a9f6b4c70c0
marios <msthoma@yahoo.com>
2020-05-18 11:12:50 +0300

    visualize_grid can now draw path if it is provided

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import numpy as np
 
 
@@ -5,14 +7,30 @@ def new_grid(dim, fill=" "):
     return np.array([[f"{fill}" for _ in range(dim)] for _ in range(dim)])
 
 
-def visualize_grid(grid, visited=None):
+def visualize_grid(grid, visited=None, path=None, start=None, goal=None):
+    grid = np.copy(grid)
     # symbols for grid visualization
-    border_sym, space_sym, visited_sym = "+", "  ", "."
+    border_sym = "+"
+    space_sym = "  "
+    visited_sym = "."
+    path_sym = "x"
+    start_sym = "s"
+    goal_sym = "g"
 
+    # mark the points visited
     if visited:
-        # mark the points already visited if they are provided
         for p in visited:
             grid[p] = visited_sym
+
+    # draw path if provided
+    if path:
+        for p in path:
+            grid[p] = path_sym
+
+    if start:
+        grid[start] = start_sym
+    if goal:
+        grid[goal] = goal_sym
 
     # add visual border at beginning and end of grid
     main_body = [space_sym.join([border_sym] + list(row) + [border_sym]) for row in grid]
@@ -22,3 +40,7 @@ def visualize_grid(grid, visited=None):
     whole = [top] + main_body + [bottom]
 
     return "\n".join(whole)
+
+
+# inspired by this gist: https://gist.github.com/hrldcpr/2012250
+def tree(): return defaultdict(tree)
