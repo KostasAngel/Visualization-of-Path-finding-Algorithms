b2d3c965e839a3ffc5678b0674d17f1ea398373d
marios <msthoma@yahoo.com>
2020-05-20 11:39:07 +0300

    dfs implementation (by modifying bfs)

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,9 +1,47 @@
+from collections import deque
+
+import utils
+
+
 def calculate(grid, start, goal):
-    pass
+    queue = deque([start])  # double ended queue, to use as a FIFO queue
+
+    visited = []
+
+    child_parent_pairs = dict()
+
+    goal_reached = False
+
+    while queue and not goal_reached:  # stops when all points have been considered or when goal is reached
+
+        current_point = queue.pop()  # get last point in queue
+
+        visited.append(current_point)  # mark point as visited
+
+        neighbors = utils.get_neighbors(current_point, grid)
+
+        for neighbor in neighbors:
+            if neighbor not in visited:  # check if already visited this point
+                queue.append(neighbor)
+                child_parent_pairs[neighbor] = current_point
+
+                goal_reached = neighbor == goal  # check if goal has been reached
+
+    path = utils.calculate_path(start, goal, child_parent_pairs)
+
+    return {"path": path, "visited": visited, "grid": grid, "start": start, "goal": goal}
 
 
 def main():
-    pass
+    grid = utils.new_grid(20)
+
+    grid[:17, 4] = "+"
+    grid[1, 1:9] = "+"
+    grid[10, 6:18] = "+"
+    grid[10:, 7] = "+"
+
+    res = calculate(grid=grid, start=(0, 0), goal=(17, 17))
+    utils.visualize_asciimatics(res)
 
 
 if __name__ == '__main__':
