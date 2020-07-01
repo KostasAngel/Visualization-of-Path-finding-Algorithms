3f08bc73803687d23c7c010886bd31af73f1b3eb
KostasAngel <37286544+KostasAngel@users.noreply.github.com>
2020-07-01 09:24:41 +0300

    Update Main.py
    Co-authored-by: msthoma <msthoma@yahoo.com>

--- Main.py
+++ Main.py
@@ -200,7 +200,7 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         maze_history = grid.get_maze_history()
         for y in range(GRIDSIZE):
             for x in range(GRIDSIZE):
-                scene.addRect(x * SIDE, y * SIDE, 10, 10,
+                scene.addRect(y * SIDE, x * SIDE, 10, 10,
                               penPoint, wallBrush)
         for x, y in maze_history:
             QtTest.QTest.qWait(2)
